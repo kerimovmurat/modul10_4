f5546794338e39a35535587b0b364b064594d759
kerimovmurat <kerimovmurat@yandex.ru>
2024-10-15 21:55:29 +0300

    Update modul10_4.py

--- modul10_4.py
+++ modul10_4.py
@@ -31,26 +31,26 @@ class Cafe:
             for table in self.tables:
                 if table.guest is None:
                     table.guest = guest
-                    print (f'{guest.name} сел(-а) за стол номер {table.number}')
-                    guest.start ()
+                    print(f'{guest.name} сел(-а) за стол номер {table.number}')
+                    guest.start()
                     sits = True
                     break
             if not sits:
-                self.queue.put (guest)
-                print (f'{guest.name} в очереди')
+                self.queue.put(guest)
+                print(f'{guest.name} в очереди')
 
     def discuss_guests(self):
-        while not queue.Empty () or any (table.guest is not None for table in self.tables):
+        while not queue.Empty() or any (table.guest is not None for table in self.tables):
             for table in self.tables:
-                if table.guest is not None and not table.guest.is_alive ():
-                    print (f'{table.guest.name} покушал(-а) и ушел(ушла)')
-                    print (f'Стол номер {table.number} свободен')
+                if table.guest is not None and not table.guest.is_alive():
+                    print(f'{table.guest.name} покушал(-а) и ушел(ушла)')
+                    print(f'Стол номер {table.number} свободен')
                     table.guest = None
-                    if not self.queue.empty ():
-                        next_guest = self.queue.get ()
+                    if not self.queue.empty():
+                        next_guest = self.queue.get()
                         table.guest = next_guest
-                        print (f'{next_guest.name} вышел(-а) из очереди и сел(-а) за стол номер {table.number}')
-                        next_guest.start ()
+                        print(f'{next_guest.name} вышел(-а) из очереди и сел(-а) за стол номер {table.number}')
+                        next_guest.start()
 
 
 # Создание столов
